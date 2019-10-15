60325c53e942c5482760b07c4769409033dc3370
stephensilva <ssilva755@gmail.com>
2019-10-14 20:17:00 -0500

    finished Tower of hanoi

---
+++ src/TowersOfHanoi/towers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(function() {
+  $('[data-block]').draggable({
+    revert: "invalid"
+  });
+$('[data-stack]').droppable({
+  drop: function(event, ui) {
+    $(ui.draggable).appendTo(this).attr('style','position: "relative"'); 
+  }
+
+});
+});
